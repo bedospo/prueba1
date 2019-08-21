be03a8a82c4bf4f6119369f18dd0288a7d3e62b2
Alumno <Alumno@PC32>
2019-08-21 12:07:49 -0500

    weeef

---
+++ prueba1/prueba1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prueba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+        }
+    }fgfg
+}
+ddff
